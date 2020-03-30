7521368688440c780279c3657394aeb9d9783fa4
d90375 <d90375@gmail.com>
2020-03-31 00:54:29 +0300

    feat: add decoder

--- task1/caesar.js
+++ task1/caesar.js
@@ -1,5 +1,43 @@
-const caesar = str => {
-  return `${str} 123`;
+const caesarEncode = (str, shift) => {
+  let newStr = '';
+  for (const i of str) {
+    const re = /[a-zA-Z]/;
+
+    if (re.test(i)) {
+      let En = i.codePointAt(0) + shift;
+      if (i.codePointAt(0) <= 90 && En > 90) {
+        En = (En % 90) + 64;
+      } else if (i.codePointAt(0) <= 122 && En > 122) {
+        En = (En % 122) + 96;
+      }
+      newStr += String.fromCharCode(En);
+    } else {
+      newStr += i;
+    }
+  }
+  return newStr;
 };
 
-module.exports = caesar;
+const caesarDecode = (str, shift) => {
+  let newStr = '';
+  for (const i of str) {
+    const re = /[a-zA-Z]/;
+    if (re.test(i)) {
+      let En = i.codePointAt(0) - shift;
+      if (i.codePointAt(0) <= 65 && En < 65) {
+        En = 26 + En;
+      } else if (i.codePointAt(0) >= 97 && En < 97) {
+        En = 26 + En;
+      }
+      newStr += String.fromCharCode(En);
+    } else {
+      newStr += i;
+    }
+  }
+  return newStr;
+};
+
+module.exports = {
+  encode: caesarEncode,
+  decode: caesarDecode
+};
--- task1/index.js
+++ task1/index.js
@@ -1,39 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-
 const { program } = require('commander');
 const caesar = require('./caesar');
-
 const parseIntHere = n => parseInt(n, 10);
 
 program
   .option('-a, --action <type>', 'action')
   .option('-i, --input <type>', 'input')
-  .option('-o, --output <type>', 'verbosity that can be increased')
+  .option('-o, --output <type>', 'verbosity')
   .option('-s, --shift <number>', 'repeatable value', parseIntHere);
-
 program.parse(process.argv);
+
+let setCaesar;
 switch (program.action) {
   case 'encode':
-    console.log('start code');
+    setCaesar = caesar.encode;
     break;
   case 'decode':
-    console.log('start decode');
+    setCaesar = caesar.decode;
     break;
   default:
-    console.error('error');
+    console.error('error decode');
 }
 
 const inputFilePath = path.join(__dirname, program.input);
-
 const outputFilePath = path.join(__dirname, program.output);
 
-fs.readFile(inputFilePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('err');
+fs.readFile(inputFilePath, 'utf-8', (inputError, data) => {
+  if (inputError) {
+    console.error(`error ${inputError} readFile`);
   }
-  fs.writeFile(outputFilePath, caesar(data), errOutput => {
-    if (errOutput) throw errOutput;
-    console.log('The file has been saved!');
+  fs.writeFile(outputFilePath, setCaesar(data, program.shift), err => {
+    if (err) throw err;
+    console.log('File saved');
   });
 });
